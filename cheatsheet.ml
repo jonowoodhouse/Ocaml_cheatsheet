768ea0f858d4fe59dbd3a8c5f42de818006f29dd
Jono Woodhouse <Jono.Woodhouse@example.com>
2020-02-08 13:19:09 +0800

    Initial commit

---
+++ cheatsheet.ml
@@ -0,0 +1,22 @@
+open Core
+
+let () = print_endline "hello world"
+
+(* TODO:
+   variables
+   records
+   tuples
+   variants
+   options
+   ref
+   List
+   set
+   hashtable
+   string
+   map + bind
+   modules
+   functors
+   phantom types
+   async
+
+*)
