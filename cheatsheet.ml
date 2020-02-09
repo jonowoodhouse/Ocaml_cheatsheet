2c3cd6b785a667e77ee134ff7544c6206f73a46e
Jono Woodhouse <Jono.Woodhouse@example.com>
2020-02-09 20:37:18 +0800

    updates

--- cheatsheet.ml
+++ cheatsheet.ml
@@ -2,20 +2,45 @@ open Core
 
 let () = print_endline "hello world"
 
+
+
 (* TODO:
+   Google Analytics and Home Page
+
    variables
-   records
+   printing to the screen
+   annotated variables and return types
+   arguments
+   records (022)
+   modules (021 023)
    tuples
-   variants
+   matching and function
+   variants (004 024 025)
+   polymorphic variants
+   fun
+   partial functions
    options
-   ref
-   List
-   set
-   hashtable
-   string
+   ref and mutable
+   List - map, fold, reduce (010 017 029 030  )
+   Set
+   Map
+   Hashtbl (027)
+   string (018 019)
    map + bind
-   modules
-   functors
+   pipes
+   In_channel (008)
+   Lazy (028)
+   Sexp (031)
+   Identifiable
+   functors (031)
    phantom types
+   compare (020)
    async
+   error handling and Or_error and Result.t (026)
+   expect tests (002)
+   Assembler & profiling (014 015 016)
+   ppx
+   Cool tricks
+   Spacemacs commands
 *)
+
